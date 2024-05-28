5b303e43c92767f74d2803c9b963e9dbe8ce7de3
Sébastien Hellebois <dogevi@gmail.com>
2024-05-28 15:02:37 -0400

    refactor: 💡 remove -webkit-transform

--- src/app/pages/components-page/pages/file-viewer/components/basic-viewer/basic-viewer.component.ts
+++ src/app/pages/components-page/pages/file-viewer/components/basic-viewer/basic-viewer.component.ts
@@ -17,7 +17,7 @@ export class BasicViewerComponent {
       name: 'image1',
       type: 'image/png',
       location:
-        'https://media.discordapp.net/attachments/1008571172792828055/1096409134896775208/c3ulnta0rk_a_goat_with_this_logo_no_background_no_text_2269a9f9-1823-4f99-b87a-e2a82218b4a1.png?width=468&height=468',
+        'https://th.bing.com/th/id/OIG3.z4YZpptEw4ARkQaBM7sF?w=1024&h=1024&rs=1&pid=ImgDetMain',
     },
     {
       name: 'image2',
