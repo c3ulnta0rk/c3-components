4683694e310250ab6833e1c1c30b8634b680afd2
Sébastien Hellebois <dogevi@gmail.com>
2024-06-25 10:24:29 -0400

    feat: 🎸 add toObservable for dialog service

--- projects/c3-components/src/lib/c3-dialog/services/c3-dialog.service.ts
+++ projects/c3-components/src/lib/c3-dialog/services/c3-dialog.service.ts
@@ -9,6 +9,8 @@ import {
   PromptConfig,
   C3PromptDialogComponent,
 } from '../components/c3-dialog-prompt.component';
+import { C3ExtandedPromise } from '../../c3-extanded-promise';
+import { lastValueFrom, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -16,12 +18,53 @@ import {
 export class C3DialogService {
   readonly #dialog = inject(MatDialog);
 
-  public confirm(data: ConfirmConfig): Promise<boolean> {
-    return new Promise((resolve, reject) => {
-      const dialogRef = this.#dialog.open(ConfirmDialogComponent, {
+  /**
+   * Opens a confirm dialog and returns a promise that resolves to a boolean indicating whether the user accepted or rejected the confirmation.
+   *
+   * @param {ConfirmConfig} data Configuration options for the confirm dialog. See ConfirmConfig below.
+   * @returns {C3ExtendedPromise<boolean>} A promise that resolves to `true` if the user accepted the confirmation, and `false` otherwise.
+   */
+  public confirm(data: ConfirmConfig) {
+    const dialogRef = this.#dialog.open(ConfirmDialogComponent, {
+      width: data.width || '250px',
+      data: {
+        text: data.text,
+        reject: {
+          color:
+            data.reject && data.reject.color ? data.reject.color : undefined,
+          text: (data.reject && data.reject.text) || 'Annuler',
+        },
+        accept: {
+          color: (data.accept && data.accept.color) || 'primary',
+          text: (data.accept && data.accept.text) || 'Accepter',
+        },
+      },
+    });
+
+    return new C3ExtandedPromise<boolean>(() =>
+      lastValueFrom(dialogRef.afterClosed().pipe(map(Boolean)))
+    );
+  }
+
+  /**
+   * Opens a prompt dialog and returns a promise that resolves to the value entered by the user, or `false` if the user cancelled the prompt.
+   *
+   * @param {PromptConfig} data Configuration options for the prompt dialog. See PromptConfig below.
+   * @returns {C3ExtendedPromise<false | T>} A promise that resolves to the value entered by the user, or `false` if the user cancelled the prompt.
+   */
+  public prompt<T = any>(data: PromptConfig): C3ExtandedPromise<false | T> {
+    const dialogRef = this.#dialog.open<C3PromptDialogComponent, any, T>(
+      C3PromptDialogComponent,
+      {
         width: data.width || '250px',
+        maxWidth: data.maxWidth || '100vw',
         data: {
           text: data.text,
+          defaultValue: data.defaultValue,
+          placeholder: data.placeholder,
+          multiline: data.multiline,
+          validators: data.validators,
+          required: data.required,
           reject: {
             color:
               data.reject && data.reject.color ? data.reject.color : undefined,
@@ -32,49 +75,14 @@ export class C3DialogService {
             text: (data.accept && data.accept.text) || 'Accepter',
           },
         },
-      });
+      }
+    );
 
-      dialogRef.afterClosed().subscribe(async (result) => {
-        if (result) resolve(true);
-        else resolve(false);
-      });
-    });
-  }
-
-  public prompt<T = any>(data: PromptConfig): Promise<false | T> {
-    return new Promise((resolve, reject) => {
-      const dialogRef = this.#dialog.open<C3PromptDialogComponent, any, T>(
-        C3PromptDialogComponent,
-        {
-          width: data.width || '250px',
-          maxWidth: data.maxWidth || '100vw',
-          data: {
-            text: data.text,
-            defaultValue: data.defaultValue,
-            placeholder: data.placeholder,
-            multiline: data.multiline,
-            validators: data.validators,
-            required: data.required,
-            reject: {
-              color:
-                data.reject && data.reject.color
-                  ? data.reject.color
-                  : undefined,
-              text: (data.reject && data.reject.text) || 'Annuler',
-            },
-            accept: {
-              color: (data.accept && data.accept.color) || 'primary',
-              text: (data.accept && data.accept.text) || 'Accepter',
-            },
-          },
-        }
-      );
-
-      dialogRef.afterClosed().subscribe(async (result) => {
-        if (result) resolve(result);
-        else resolve(false);
-      });
-    });
+    return new C3ExtandedPromise<false>(() =>
+      lastValueFrom(
+        dialogRef.afterClosed().pipe(map((value) => value || false))
+      )
+    );
   }
 
   createDialgFromComponent<T>(
---
+++ projects/c3-components/src/lib/c3-extanded-promise.ts
@@ -0,0 +1,21 @@
+import { Observable } from 'rxjs';
+
+export class C3ExtandedPromise<T> extends Promise<T> {
+  constructor(
+    executor: (
+      resolve: (value: T | PromiseLike<T>) => void,
+      reject: (reason?: any) => void
+    ) => void
+  ) {
+    super(executor);
+  }
+
+  public toObservable(): Observable<T> {
+    return new Observable<T>((observer) => {
+      this.then((value) => {
+        observer.next(value);
+        observer.complete();
+      }).catch((err) => observer.error(err));
+    });
+  }
+}
