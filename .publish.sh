8caa6b26287f358d9cff2d8cfcd4351a366b56f4
Kroenen62 <dogevi@gmail.com>
2024-07-06 11:24:42 -0400

    docs: ✏️ try to correct doc

--- .publish.sh
+++ .publish.sh
@@ -1,6 +1,9 @@
 git add . \
 && git-cz \
 && git push \
+&& bunx @angular/cli build c3-components-docs \
+&& rm -rf docs \
+&& cp -r dist/c3-components-docs/browser docs \
 && bunx @angular/cli build c3-components \
 && cd dist/c3-components \
 && npm publish
