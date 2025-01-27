8dae2e4e4ad12d5fdd6a05d74aa63010625b139c
Sébastien Hellebois <dogevi@gmail.com>
2025-01-27 10:08:06 -0500

    docs: ✏️ deploy doc

--- .publish-doc.sh
+++ .publish-doc.sh
@@ -1,4 +1,5 @@
-bunx @angular/cli build --base-href /c3-components/ c3-components-docs \
-&& git add . \
-&& git-cz \
-&& git push
+#!/bin/bash
+bunx @angular/cli build --base-href /c3-components/ c3-components-docs 
+git add . 
+git-cz 
+git push
