f2f5ed641f0557fb33d15b00dcfe542c5ee4daaf
Sébastien Hellebois <dogevi@gmail.com>
2026-01-27 09:32:58 -0500

    refactor: 💡 reorder order of declaration

--- projects/c3-components/src/lib/c3-file-displayer/c3-file-display-list/c3-file-display-list-column-def.directive.ts
+++ projects/c3-components/src/lib/c3-file-displayer/c3-file-display-list/c3-file-display-list-column-def.directive.ts
@@ -1,23 +1,5 @@
-import { contentChild, Directive, inject, input, TemplateRef } from '@angular/core';
+import { contentChild, Directive, forwardRef, inject, input, TemplateRef } from '@angular/core';
 import { C3_FILE_DISPLAY_LIST } from './tokens';
-
-@Directive({
-  selector: '[c3FileDisplayListColumnDef]',
-  standalone: true,
-})
-export class C3FileDisplayListColumnDefDirective {
-  public readonly title = input.required<string>({
-    alias: 'c3FileDisplayListColumnDef',
-  });
-  public readonly classList = input<string[]>([]);
-  private readonly _table = inject(C3_FILE_DISPLAY_LIST, { optional: true });
-
-  // contentChild searches in projected content (via ng-content)
-  // Note: contentChild may not find directives on ng-template in tests, but works in real usage
-  // when directives are projected via ng-content in the parent component
-  public readonly headerCellDef = contentChild(C3FileDisplayListHeaderCellDefDirective, { descendants: true });
-  public readonly cellDef = contentChild(C3FileDisplayListCellDefDirective, { descendants: true });
-}
 
 @Directive({
   selector: '[c3FileDisplayListHeaderCellDef]',
@@ -33,4 +15,26 @@ export class C3FileDisplayListHeaderCellDefDirective {
 })
 export class C3FileDisplayListCellDefDirective {
   template = inject<TemplateRef<any>>(TemplateRef);
+}
+
+@Directive({
+  selector: '[c3FileDisplayListColumnDef]',
+  standalone: true,
+})
+export class C3FileDisplayListColumnDefDirective {
+  public readonly title = input.required<string>({
+    alias: 'c3FileDisplayListColumnDef',
+  });
+  public readonly classList = input<string[]>([]);
+  private readonly _table = inject(C3_FILE_DISPLAY_LIST, { optional: true });
+
+  // contentChild searches in projected content (via ng-content)
+  // Note: contentChild may not find directives on ng-template in tests, but works in real usage
+  // when directives are projected via ng-content in the parent component
+  public readonly headerCellDef = contentChild(forwardRef(() => C3FileDisplayListHeaderCellDefDirective), {
+    descendants: true,
+  });
+  public readonly cellDef = contentChild(forwardRef(() => C3FileDisplayListCellDefDirective), {
+    descendants: true,
+  });
 }
